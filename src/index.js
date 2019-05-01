dc88928c6bbea0b6025deb8bc95a03a675720da9
mbonaci <mbonaci@gmail.com>
2019-05-01 21:05:25 +0200

    Fix unexpected token

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : '0';
     this.setState({
       history: history.concat([{
-        squares: /*squares,*/
+        squares /*squares,*/
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
