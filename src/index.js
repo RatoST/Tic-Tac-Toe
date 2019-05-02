83bc57094aa33e4a5f9c0233f2cd00c0401434cf
mbonaci <mbonaci@gmail.com>
2019-05-02 09:24:19 +0200

    Pull out Board in a separate file

--- src/index.js
+++ src/index.js
@@ -2,56 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import PropTypes from 'prop-types';
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-    {props.value}
-    </button>
-  );
-}
-
-Square.propTypes = {
-  onClick: PropTypes.eventHandler,
-  value: PropTypes.number,
-};
-
-
-class Board extends React.Component {
-
-  renderSquare(i) {
-    return (
-      <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
-    );
-  }
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-        </div>
-      </div>
-  );
-  }
-}
-
-Board.propTypes = {
-  onClick: PropTypes.eventHandler,
-  squares: PropTypes.number,
-};
+import Board from './components/Board';
 
 function calculateWinner(squares) {
   const lines = [
